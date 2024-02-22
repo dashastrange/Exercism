f888de8d58a3247bbb8c5114e43cb9b9a22bdbb2
dashastrange <dashastrange@gmail.com>
2024-02-22 16:52:52 +0100

    Lasagna solved

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,10 +1,27 @@
 class Lasagna
 {
+
     // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven() => 40;
 
     // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int minutesAleadyInOven)
+    {
+        return ExpectedMinutesInOven() - minutesAleadyInOven;
+    }
 
     // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int layers)
+    {
+        return layers*2;
+    }
 
     // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int layers, int minutesAleadyInOven)
+    {
+        return PreparationTimeInMinutes(layers) + minutesAleadyInOven;
+    }
+
 }
+
+    

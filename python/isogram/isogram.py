ffeb0708165abd7bbd97ae88e008817ce79de9bd
vadariava <dashastrange@gmail.com>
2025-03-11 01:48:55 +0100

    Ternary operator used in Isogram exercise

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,7 +1,4 @@
 def is_isogram(string):
    clean_string = string.lower().replace("-","").replace(" ","")
 
-   if len(clean_string) != len(set(clean_string)):
-      return False
-   else:
-      return True
+   return len(clean_string) == len(set(clean_string))

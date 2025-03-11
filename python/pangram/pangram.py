97bfd5ad7e558a9c863467cab0f41e61a2e2b2d8
vadariava <dashastrange@gmail.com>
2025-03-11 01:03:15 +0100

    Pangram beautified

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,33 +1,8 @@
 def is_pangram(sentence):
-    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    letters_sentence = []
-    identificator = []
-    x = 0
+    alphabet = set('abcdefghijklmnopqrstuvwxyz')
+    letters_sentence = set()
 
-    for l in sentence.lower():
-        if l in alphabet:
-            letters_sentence.append(l)
-    #print(letters_sentence)
-    
-    while x in range(len(alphabet)):
-        if alphabet[x] in letters_sentence:
-            x += 1
-            identificator.append('+')
-            continue
-        #print(identificator)
-    
-        if alphabet[x] not in letters_sentence:
-            x += 1 
-            identificator.append('-')
-            continue
-        #print(identificator)
-
-    if '-' in identificator:
-        #print('False')
-        return False
-    else:
-        #print('True')
-        return True
-        
-#is_pangram("abcdefghijklmnopqrstuvwxy")
-#is_pangram("the_quick_brown_fox_jumps_over_the_lazy_dog")
+    for char in sentence.lower():
+        if char in alphabet:
+            letters_sentence.add(char)
+    return len(letters_sentence) == len(alphabet)

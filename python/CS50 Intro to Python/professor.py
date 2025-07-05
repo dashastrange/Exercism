c47b568ce340f93ab3067ecf20c4aff6721bdb0f
dashastrange <dashastrange@gmail.com>
2025-07-05 22:08:14 +0200

    Professor problem started

--- python/CS50 Intro to Python/professor.py
+++ python/CS50 Intro to Python/professor.py
@@ -1,16 +1,24 @@
 import random
 
+levels = [1, 2, 3]
 
 def main():
     ...
 
 
 def get_level():
-    ...
+    while True:
+        level = input("Level: ")
+
+        if level in levels:
+            break
+        else:
+            continue
+    return level
 
 
 def generate_integer(level):
-    ...
+
 
 
 if __name__ == "__main__":

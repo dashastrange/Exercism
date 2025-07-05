c3e764d5d448074a19d883bd648092a45345fa2d
dashastrange <dashastrange@gmail.com>
2025-07-06 00:36:42 +0200

    Solved Professor

--- python/CS50 Intro to Python/professor.py
+++ python/CS50 Intro to Python/professor.py
@@ -1,24 +1,60 @@
 import random
 
-levels = [1, 2, 3]
-
 def main():
-    ...
+    rounds = 0
+    score = 0
+    tries = 2
+
+    lvl = get_level()
+
+    while rounds != 10:
+
+        x = generate_integer(lvl)
+        y = generate_integer(lvl)
+
+        result = int(x + y)
+        rounds += 1
+
+        while True:
+            guess = int(input(f"{x} + {y} = "))
+
+            if tries > 0:
+                if result == guess:
+                    score += 1
+                    break
+                if result != guess:
+                    tries -= 1
+                    print("EEE")
+                    continue
+            else:
+                print(f"{x} + {y} = {result}")
+                tries = 2
+                break
+    print("Score: ", score)
 
 
 def get_level():
+    levels = [1, 2, 3]
+
     while True:
-        level = input("Level: ")
+        try:
+            level = int(input("Level: "))
+        except ValueError:
+            continue
 
         if level in levels:
-            break
+            return level
         else:
             continue
-    return level
 
-
-def generate_integer(level):
-
+def generate_integer(l):
+    if l == 1:
+        rand = random.randint(0, 9)
+    elif l == 2:
+        rand = random.randint(10, 99)
+    elif l == 3:
+        rand = random.randint(100, 999)
+    return rand
 
 
 if __name__ == "__main__":
